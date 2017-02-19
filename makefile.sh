47779dd31d8a3285dee6f6039ad616f995b84228
jreyn014 <jreyn014@ucr.edu>
2017-02-19 00:37:24 +0000

    fifth commit

---
+++ makefile.sh
@@ -0,0 +1,8 @@
+flags=-Wall -Werror -ansi -pedantic
+
+all :
+		if [ ! -d bin ]; then mkdir ./bin; fi
+		g++ $(flags) src/main.cpp -o bin/rshell
+
+rshell :
+		g++ $(flags) src/main.cpp -o bin/rshell
